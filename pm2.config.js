873dd1c3b70c815d77f9df54d581fb5f1123ea8f
shangtengfei <shangtengfei@aixuexi.com>
2024-06-27 16:33:09 +0800

    fix

--- pm2.config.js
+++ pm2.config.js
@@ -1,13 +1,29 @@
 module.exports = {
     apps : [{
-      name   : "Interest-tool",
+      name   : "interest-tool",
       script : "./dist/main.js",
       env: {
-        "NODE_ENV": "production",
-        "PORT": 3000,
+        "NODE_ENV": "development",
       },
+      env_production: {
+        NODE_ENV: 'production'
+      },
+      instances : "max",
+      exec_mode : "cluster",
       error_file: './logs/app-err.log',
       out_file: './logs/app-out.log',
       log_date_format: 'YYYY-MM-DD HH:mm:ss',
+      deploy: {
+        production : {
+          "user" : "root",
+          "host" : ["119.3.187.4"],
+          "ref"  : "origin/master",
+          "repo" : "git@gitee.com:sh_tengfei/interest-tool.git",
+          "path" : "/home/www/interest-tool/",
+          'pre-deploy-local': '',
+          'post-deploy': 'npm install && pm2 reload pm2.config.js --env production',
+          'pre-setup': ''
+        }
+      }
     }]
   }
